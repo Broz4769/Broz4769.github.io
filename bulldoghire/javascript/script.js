6a36b47a96b3c9ee84840129c07ea74d0f14df0e
Broz4769 <68515464+Broz4769@users.noreply.github.com>
2021-12-09 00:17:57 +0200

    Update script.js
    gallery - Self Transition. removed arrows

--- bulldoghire/javascript/script.js
+++ bulldoghire/javascript/script.js
@@ -1,19 +1,18 @@
 /* 1. Gallery */
 
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
+carousel();
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
+function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";  
-}
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 4000); // Change image every 2 seconds
+} 
+
+
