f85657920c5ea8473a6e7466b3ed55ae15564751
Broz4769 <bwbulling@hotmail.com>
2023-03-16 11:38:34 +0200

    Viewport resolution
    display viewport resolution

---
+++ bulldoghire/javascript/viewportscript.js
@@ -0,0 +1,7 @@
+/* 2. Modal Menu Tab */
+// Get the modal
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+var x = document.getElementById("demo");
+x.innerHTML = "Browser width: " + w + ", height: " + h + ".";
