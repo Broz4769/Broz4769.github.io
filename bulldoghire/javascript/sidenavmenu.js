c81ee6d09b22585877473ad2543e5b6299602237
Broz4769 <bwbulling@hotmail.com>
2023-03-21 03:39:23 +0200

    Edit Sidenav.js File
    Removed sidenav_responsive javascript and text.

--- bulldoghire/javascript/sidenavmenu.js
+++ bulldoghire/javascript/sidenavmenu.js
@@ -1,15 +1,25 @@
-/*1. Desktop View*/
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+/*1. Sidenav menu*/
+    /* Open the sidenav */
+    var width = window.matchMedia("(min-width: 1025px)");
+
+    function openNav() {
+      if (width.matches) {
+      document.getElementById("mySidenav").style.width = "350px";
+      document.getElementById("main").style.marginLeft = "350px";
+      }
+      else {
+      document.getElementById("mySidenav").style.width = "100%";
+    }}
+    
+    /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+      
   }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+
+
+
 
 
 
